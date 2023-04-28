40b228bd264a868808348e940c02bbfa4f0bb672
Federico Bagnalasta <federico.bagnalasta@gmail.com>
2023-04-28 22:34:13 +0200

    test

--- RicOperativa/src/it/unibs/ricoperativa/Main.java
+++ RicOperativa/src/it/unibs/ricoperativa/Main.java
@@ -1,10 +1,20 @@
 package it.unibs.ricoperativa;
 
+import java.io.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
+		int m[][], r_i[], s_j[], c, k, alfa_j, h, x, y;
+		readFile();
 
+	}
+	
+	/*
+	 * Metodo per la lettura del file che contiene i dati del problema
+	 */
+	private static void readFile() throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader("coppia_11.txt"));
 	}
 
 }
